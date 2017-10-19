71e23fdff31bdc080a4592d3b79b4354c8cf07bb
lucas <lucas@b3consultoria.com.br>
2017-10-19 16:26:52 -0200

    oi

--- Sample.Android/AlterarSituacaoOprActivity.cs
+++ Sample.Android/AlterarSituacaoOprActivity.cs
@@ -1,4 +1,5 @@
 ﻿using Android.App;
+using r = Android.Content.Res;
 using Android.OS;
 using Android.Views;
 using Android.Widget;
@@ -10,6 +11,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Drawing;
 
 namespace Sample.Android
 {
@@ -81,12 +83,16 @@ namespace Sample.Android
                 };
 
                 scrollView.AddView(mainLayout);
-
-
+                
+                LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MatchParent, LinearLayout.LayoutParams.WrapContent);
+                linearLayoutParams.SetMargins(10, 0, 10, 0);
+                linearLayoutParams.Weight = Convert.ToSingle(0.5);
 
                 for (int n = 0; n < DadosRelatorioCesv.ListaDestinos.Count -1; n++)
                 {
                     var aButton = new Button(this);
+                    aButton.LayoutParameters = linearLayoutParams;
+                    aButton.SetTextColor(new r.ColorStateList(new int[][] { new int[] { } }, new int[] { Color.White.ToArgb() }));
                     aButton.Id = Convert.ToInt32(DadosRelatorioCesv.ListaDestinos[n].statusId);
                     aButton.Text = DadosRelatorioCesv.ListaDestinos[n].denominacao;
                     aButton.Click += delegate (object sender, EventArgs e)
