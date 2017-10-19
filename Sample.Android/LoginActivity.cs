222888c77a5d9d854c94368430951df762c6db46
lucas <lucas@b3consultoria.com.br>
2017-10-19 14:28:11 -0200

    oi

--- Sample.Android/LoginActivity.cs
+++ Sample.Android/LoginActivity.cs
@@ -11,7 +11,7 @@ using System.Text;
 
 namespace Sample.Android
 {
-    [Activity(Label = "LoginActivity")]
+    [Activity(Label = "Ações")]
     public class LoginActivity : Activity
     {
         protected async override void OnCreate(Bundle bundle)
@@ -47,7 +47,11 @@ namespace Sample.Android
             for (int n = 0; n < permissoes.Count ; n++)
             {
                 var aButton = new Button(this);
-                var aLayout = new Button(this);
+
+                //LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WrapContent);
+                //linearLayoutParams.SetMargins(0, 0, 10, 10);
+                //linearLayoutParams.Weight = Convert.ToSingle(0.5);
+                //aButton.LayoutParameters = linearLayoutParams;
 
                 switch (permissoes[n].denominacao)
                 {
